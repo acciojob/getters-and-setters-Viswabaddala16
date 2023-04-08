d6d4f1dd80a675811e812840c1ff743b5572a1de
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-04-08 21:25:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+		get getName(){
+			return this.name;
+		}
+		set changeAge(newAge){
+			return this.newAge;
+		}
+		
+	}
+	
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(+this.name+"is studying");
+	}
+}
 
-class Teacher extends Person {}
-
+class Teacher extends Person {
+	teach(){
+		console.log(+this.name+"is teaching");
+	}
+}
+var student1 = new Student("viswa","22");
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
