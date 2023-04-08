75e6163fa38eb136c194e71ba59afdfc2a6b9ac3
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-04-08 21:52:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this.name = name;
-		this.age = age;
-		get getName(){
-			return this.name;
-		}
-		set changeAge(newAge){
-			return this.newAge;
-		}
-		
-	}
-	
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  /**
+   * @param {any} age
+   */
+  set age(age) {
+    this._age = age;
+  }
 }
-
+ 
 class Student extends Person {
-	study(){
-		console.log(+this.name+"is studying");
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
-
+ 
 class Teacher extends Person {
-	teach(){
-		console.log(+this.name+"is teaching");
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-const person = new Person("viswa","22");
-// Do not change the code below this line
+ 
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
